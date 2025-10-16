d1c557eb78e9a01301cba33538b7c33bdac03455
Lukas Voegl <lukas@voegl.org>
2025-10-16 21:34:38 +0200

    build(Deps): Bump publish plugin from 0.7.+ to 0.8.1

--- stonecutter.gradle.kts
+++ stonecutter.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     id("dev.kikugie.stonecutter")
     id("fabric-loom") version "1.10-SNAPSHOT" apply false
-    id("me.modmuss50.mod-publish-plugin") version "0.7.+" apply false
+    id("me.modmuss50.mod-publish-plugin") version "0.8.1" apply false
     id("com.github.johnrengelman.shadow") version "8.1.1" apply false
 }
 stonecutter active "1.21.4" /* [SC] DO NOT EDIT */
